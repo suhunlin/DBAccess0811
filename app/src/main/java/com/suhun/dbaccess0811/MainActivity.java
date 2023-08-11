bfa0ede02fdeefe7adcaad02e44584ddd0093a37
suhunlin <lin.suhun@msa.hinet.net>
2023-08-11 14:27:26 +0800

    Initial TextView and EditText

--- app/src/main/java/com/suhun/dbaccess0811/MainActivity.java
+++ app/src/main/java/com/suhun/dbaccess0811/MainActivity.java
@@ -4,13 +4,26 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private String tag = MainActivity.class.getSimpleName();
+    private TextView view_result;
+    private EditText view_name, view_tel, view_birthday;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        initView();
+    }
+
+    private void initView(){
+        view_result = findViewById(R.id.lid_queryResult);
+        view_name = findViewById(R.id.lid_nameInput);
+        view_tel = findViewById(R.id.lid_telInput);
+        view_birthday = findViewById(R.id.lid_birthdayInput);
     }
 
     public void queryFun(View view){
