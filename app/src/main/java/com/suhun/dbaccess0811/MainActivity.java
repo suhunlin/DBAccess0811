eb7dd48dbd10e22846fed22bf4f6636c3ac5d338
suhunlin <lin.suhun@msa.hinet.net>
2023-08-11 14:22:03 +0800

    Add layout

--- app/src/main/java/com/suhun/dbaccess0811/MainActivity.java
+++ app/src/main/java/com/suhun/dbaccess0811/MainActivity.java
@@ -3,6 +3,7 @@ package com.suhun.dbaccess0811;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +11,12 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void queryFun(View view){
+
+    }
+    public void insertFun(View view){
+
     }
 }
