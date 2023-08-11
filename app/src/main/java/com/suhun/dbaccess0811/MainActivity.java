5615ea2f175c8d649116c097c3a843859e3e7c2e
suhunlin <lin.suhun@msa.hinet.net>
2023-08-11 14:51:22 +0800

    Add MyDBOpenHelper and initial db

--- app/src/main/java/com/suhun/dbaccess0811/MainActivity.java
+++ app/src/main/java/com/suhun/dbaccess0811/MainActivity.java
@@ -2,6 +2,7 @@ package com.suhun.dbaccess0811;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
@@ -11,12 +12,15 @@ public class MainActivity extends AppCompatActivity {
     private String tag = MainActivity.class.getSimpleName();
     private TextView view_result;
     private EditText view_name, view_tel, view_birthday;
+    private MyDBOpenHelper myDBOpenHelper;
+    private SQLiteDatabase db;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         initView();
+        initDB();
     }
 
     private void initView(){
@@ -24,6 +28,10 @@ public class MainActivity extends AppCompatActivity {
         view_name = findViewById(R.id.lid_nameInput);
         view_tel = findViewById(R.id.lid_telInput);
         view_birthday = findViewById(R.id.lid_birthdayInput);
+    }
+    private void initDB(){
+        myDBOpenHelper = new MyDBOpenHelper(this, "suhunDB", null, 1);
+        db = myDBOpenHelper.getWritableDatabase();
     }
 
     public void queryFun(View view){
---
+++ app/src/main/java/com/suhun/dbaccess0811/MyDBOpenHelper.java
@@ -0,0 +1,25 @@
+package com.suhun.dbaccess0811;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class MyDBOpenHelper extends SQLiteOpenHelper {
+    private final String createTable = "CREATE TABLE cust (cid INTEGER PRIMARY KEY AUTOINCREMENT, " +
+            "cname TEXT, ctel TEXT, cbirthday DATE)";
+    public MyDBOpenHelper(@Nullable Context context, @Nullable String name, @Nullable SQLiteDatabase.CursorFactory factory, int version) {
+        super(context, name, factory, version);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        db.execSQL(createTable);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+
+    }
+}
